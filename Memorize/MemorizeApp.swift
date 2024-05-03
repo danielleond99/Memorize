c84e3c9707e45e67b3d595a59aabe9b2763799d0
danielleond99 <87273355+danielleond99@users.noreply.github.com>
2024-05-03 12:42:00 -0400

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Daniel Leon on 3/5/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
