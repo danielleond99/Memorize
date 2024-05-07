fa520cba88e9122bb39f1a82ce523cee3c9dd3b7
danielleond99 <87273355+danielleond99@users.noreply.github.com>
2024-05-07 15:17:02 -0400

    create CardView

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,13 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            CardView(text: "1")
+            CardView(text: "2")
+            CardView(text: "3")
+            CardView(text: "4")
         }
+        .foregroundColor(.orange)
         .padding()
+    }
+}
+
+struct CardView: View {
+    var text: String = ""
+    
+    var body: some View {
+        ZStack{
+            RoundedRectangle(cornerRadius: 12).foregroundColor(.white)
+            RoundedRectangle(cornerRadius: 12).strokeBorder(lineWidth: 2)
+            Text(text).font(.largeTitle)
+        }
+       
     }
 }
 
